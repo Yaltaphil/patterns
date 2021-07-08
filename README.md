d339031a5250665b70d404f0b602d91e48a85629
Yaltaphil <yaltaphil@yandex.ru>
2021-07-08 12:35:30 +0300

    chain pattern +diagram

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # patterns
 
-### Object-Oriented Design Patterns
+## Object-Oriented Design Patterns
 
 *A collection of diagrams + examples, written on Typescript.*
 
 + Creational
 + Structural
 + Behavioural
-
-
--- behavioural/chain/myChain.ts
+++ behavioural/chain/myChain.ts
@@ -1,18 +1,18 @@
 /**поведенческий паттерн проектирования, который позволяет передавать запросы последовательно по цепочке обработчиков. Каждый последующий обработчик решает, может ли он обработать запрос сам и стоит ли передавать запрос дальше по цепи */
 
 interface IDrink {
-    askNext(drink: IDrink): IDrink;
+    whatElse(drink: IDrink): IDrink;
     ask(question: string): string;
 }
 abstract class Drink implements IDrink {
     private next: IDrink;
-    public askNext(drink: IDrink): IDrink {
+    public whatElse(drink: IDrink): IDrink {
         this.next = drink;
         return drink;
     }
     public ask(question: string): string {
         if (this.next) return this.next.ask(question);
-        return null;
+        return 'никто не хочет';
     }
 }
 class Beer extends Drink {
@@ -34,26 +34,24 @@ class Wine extends Drink {
     }
 }
 
+//test
 const beer = new Beer();
 const cognac = new Cognac();
 const wine = new Wine();
-wine.askNext(cognac).askNext(beer);
-const drinks = ['вино', 'чай', 'пиво', 'коньяк'];
+wine.whatElse(cognac).whatElse(beer);
+const drinks = ['кофе', 'вино', 'пиво', 'коньяк'];
 for (const drink of drinks) {
     console.log(`Кто будет ${drink}?`);
     const answer = wine.ask(drink);
-    if (answer) {
-        console.log(`${answer}`);
-    } else {
-        console.log(`никто не хочет`);
-    }
+    console.log(`${answer}`);
 }
 /* output
+Кто будет кофе?
+никто не хочет
 Кто будет вино?
 Лена:  я буду вино.
-Кто будет чай?
-никто не хочет
 Кто будет пиво?
 Петя: я буду пиво.
 Кто будет коньяк?
-Вася:  я буду коньяк.*/
+Вася:  я буду коньяк.
+*/
