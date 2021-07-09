8b29fc4677b82c067d783a4e24c10343ef773a69
Yaltaphil <yaltaphil@yandex.ru>
2021-07-09 13:06:12 +0300

    add: command pattern

--- behavioural/command/myCommand.ts
+++ behavioural/command/myCommand.ts
@@ -1,2 +1,36 @@
-/**это поведенческий паттерн проектирования, который превращает запросы в объекты, позволяя передавать их как аргументы при вызове методов, ставить запросы в очередь, логировать их, а также поддерживать отмену операций */
+/*поведенческий паттерн - превращает запросы в объекты, позволяя передавать их как аргументы при вызове методов*/
+interface IOrder {
+    take(): void;
+}
+class Order implements IOrder {
+    constructor(private receiver: Shef, private course: string) {}
+    public take(): void {
+        console.log(`Ваш заказ (${this.course}) принят. Передаю на кухню`);
+        this.receiver.prepare(this.course);
+    }
+}
+class Shef {
+    public prepare(course: string): void {
+        console.log(`Повар: заказ получен - готовлю (${course}.)`);
+    }
+}
+class Waiter {
+    private order: Order;
+    public writeOrder(order: Order): Waiter {
+        this.order = order;
+        return this;
+    }
+    public serveClient(): void {
+        this.order.take();
+    }
+}
 
+//test
+const waiter = new Waiter();
+const shef = new Shef();
+const order = new Order(shef, 'стандартный обед');
+waiter.writeOrder(order).serveClient();
+/* output
+Ваш заказ (стандартный обед) принят. Передаю на кухню
+Повар: заказ получен - готовлю (стандартный обед.)
+*/
