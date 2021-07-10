00ee867fd9d3705580829e211fecd43c346d56c0
Yaltaphil <yaltaphil@yandex.ru>
2021-07-10 15:42:34 +0300

    add: template method  pattern

---
+++ behavioural/template/myTemplate.ts
@@ -0,0 +1,40 @@
+//поведенческий паттерн проектирования, который определяет скелет алгоритма, перекладывая ответственность за некоторые его шаги на подклассы
+
+abstract class Company {
+    public templateMethod(): void {
+        this.cheef();
+        this.staff();
+        this.frelancer();
+    }
+    protected cheef(): void {
+        console.log('Сеньор: я делаю основную часть работы');
+    }
+    protected abstract staff(): void;
+    protected frelancer(): void { }
+}
+
+class Middle extends Company {
+    protected staff(): void {
+        console.log('Мидл я помогаю сеньору');
+    }
+}
+
+class Frelancer extends Company {
+    protected staff(): void {
+        console.log('Фрилансер - по необходимости');
+    }
+    protected frelancer(): void {
+        console.log('Учавствую периодически');
+    }
+}
+
+//test
+(new Middle()).templateMethod();
+(new Frelancer()).templateMethod();
+
+/* output
+Сеньор: я делаю основную часть работы
+Мидл я помогаю сеньору
+Сеньор: я делаю основную часть работы
+Фрилансер - по необходимости
+Учавствую периодически*/
